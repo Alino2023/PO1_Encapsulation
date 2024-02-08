6b7eb323c4981fd376d99009388a58e7aaeedb0c
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2024-02-08 14:27:39 -0500

    Jeu_v_20240208
    Le jeu compile correctement --- reste à limiter les points de vie à 10

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,24 +2,63 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new Personnage(); // creation de l'objet joeur
+        Personnage dragon = new Personnage(); // creation de l'objet dragon
         public Form1()
         {
-            InitializeComponent();
-        }
 
+            InitializeComponent();
+
+            joueur.PointDeVie = 10;
+            dragon.PointDeVie = joueur.PointDeVie * 3;
+            joueur.PointsDePuissance = 1;
+            dragon.PointsDePuissance = joueur.PointsDePuissance * 3;
+
+        }
+        //creation d'une fonction qui actualise les données dans le form
+        private void ActualiserLabel()
+        {
+            lbl_pointsVieJoueur.Text = " Point de vie :" +
+                joueur.PointDeVie.ToString();
+
+            lbl_pointsVieDragon.Text = "Point de vie :" +
+                dragon.PointDeVie.ToString();
+
+            lbl_puissanceAttaqueJoueur.Text = " Puissance d'attaque :" +
+                joueur.PointsDePuissance.ToString();
+
+            lbl_puissanceAttaqueDragon.Text = " Puissance d'attaque :" +
+                dragon.PointsDePuissance.ToString();
+        }
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
-
+            dragon.PointDeVie -= joueur.PointsDePuissance;
+            TourDragon();
+            ActualiserLabel();
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
-
+            joueur.PointDeVie += 5;
+            TourDragon();
+            ActualiserLabel();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            joueur.PointsDePuissance += 1;
+            TourDragon();
+            ActualiserLabel();
+        }
 
+        private void TourDragon()
+        {
+            joueur.PointDeVie -= dragon.PointsDePuissance;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActualiserLabel();
         }
     }
 }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage  //   internal class Personnage  **changer internal en public
+    {
+        //On crée les attributs de la classe Personnage
+        private string nom;
+        private int pointDeVie;
+        private int pointsDePuissance;
+
+        public string Nom { get => nom; set => nom = value; }
+        public int PointDeVie
+        // Mise en place de l'encapsulation sur l'attribut pointsDeVie
+        // Si le pointsDeVie est supérieur à 0, on affecte la valeur 
+        // Si le pointsDeVie est inférieur à 0, on affecte la valeur 0
+        {
+            get => pointDeVie;
+            set
+            {
+
+                if (value > 0) //Si le pointsDeVie est supérieur à 0, on affecte la valeur
+                {
+                    pointDeVie = value;
+                }
+                else // Si le pointsDeVie est inférieur à 0, on affecte la valeur 0
+                {
+                    pointDeVie = 0;
+                }
+            }
+        }   
+        public int PointsDePuissance { get => pointsDePuissance; set => pointsDePuissance = value; }
+
+        public bool EstEnVie
+        {
+            get
+            {
+                if (pointDeVie > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
